a4a417e61b166a0ca5de0d202f270764f7c58774
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-10-07 19:03:12 +0300

    java

--- 01_Basic java/Day 02 - 07.10.2018/Lesson code/02_Matrix/App.java
+++ 01_Basic java/Day 02 - 07.10.2018/Lesson code/02_Matrix/App.java
@@ -2,17 +2,7 @@
 public class App {
 
 	public static void main(String[] args) {
-		/*
-		int[][] grades={{99,85,100},{72,99,60}};
-
-		for(int row=0; row<grades.length;row++){
-			System.out.println("--------Grades for student: "+row+"----------");
-			for(int col=0; col<grades[row].length;col++){
-				System.out.println(grades[row][col]);
-			}
-		}*/
-		
-		
+				
 		char[][] matrix=new char[3][3];
 		for(int row=0; row<matrix.length;row++){
 			for(int col=0; col<matrix[row].length;col++){
