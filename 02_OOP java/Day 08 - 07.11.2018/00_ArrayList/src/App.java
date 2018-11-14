b0ba5fb62270268f88edc658e81415bba007b630
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-11-14 21:05:07 +0200

    java

--- 02_OOP java/Day 08 - 07.11.2018/00_ArrayList/src/App.java
+++ 02_OOP java/Day 08 - 07.11.2018/00_ArrayList/src/App.java
@@ -5,12 +5,11 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		//יצירת מערך דינמי - שניתן להוסיף לו תאים ככל שנרצה
-		//כל תא חייב להיות מחרוזת
+	
 		ArrayList<String> names = new ArrayList<>();
 		
 	
-		//------------הוספה של תא חדש למערך הדינמי-------------------
+		//------------way to add new elements to the list-------------------
 		names.add("aa");
 		names.add("aa");
 		names.add("cc");
@@ -20,15 +19,15 @@ public class App {
 		names.add(123+"");   
 		
 		
-		//------------ גישה לתא מסויים במערך הדינמי-------------------
+		//------------ access a sprcific elrment in the array by its index-------------------
 		System.out.println("item in index 3: " + names.get(3));  //--> dd
 		
 		
-		//------------ גישה לאורך המערך הדינמי-------------------------
-		System.out.println("Total Items: " + names.size());  //--> 5
+		//------------ get the size of the list -------------------------
+		System.out.println("Total Items: " + names.size());  //--> 6
 
 		
-		//------------ ריצה והצגת כל התאים במערך הדינמי-------------------
+		//------------ print all the elements in the list -------------------
 		System.out.print("All Items: ");
 		for (int i = 0; i < names.size(); i++) {
 			System.out.print(names.get(i) + " ");
@@ -38,7 +37,7 @@ public class App {
 		
 		
 		
-		//----------הסרה של תא מסויים במערך הדינמי באמצעות האינדקס של התא שנרצה להסיר -------------------
+		//----------remove a specific element by its index -------------------
 		names.remove(3); // Removes item with index number 3
 		
 		
@@ -53,16 +52,16 @@ public class App {
 		
 		
 		
-		System.out.println("Total Items: " + names.size());  //--> 4
+		System.out.println("Total Items: " + names.size());  //--> 5
 		
 		
-		//----------הסרה של תא מסויים במערך הדינמי באמצעות התוכן של התא שנרצה להסיר -------------------
-		//במקרה ויש כמה תאים עם תוכן זהה לתוכן המבוקש, יוסר התא הראשון
-		// remove - returns true if this list contained the specified element	
+		//----------remove a specific element by its content -------------------
+		// remove - returns true if this list contained the specified element
+		//if we have a few elements with the same value - the first element will be removed
 		names.remove("123");
 		
 		
-		//הסרה של כל התאים המכילים את הערך המבוקש - באמצעות לולאה
+		//remove all elements with "aa" content - with a while loop
 		while(names.remove("aa"));
 		
 		
