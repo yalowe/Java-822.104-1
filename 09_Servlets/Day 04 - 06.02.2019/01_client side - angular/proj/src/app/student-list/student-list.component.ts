b53d15d1b711be43b16c462e479532c5b4847b2f
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2019-02-06 21:50:17 +0200

    java

--- 09_Servlets/Day 04 - 06.02.2019/01_client side - angular/proj/src/app/student-list/student-list.component.ts
+++ 09_Servlets/Day 04 - 06.02.2019/01_client side - angular/proj/src/app/student-list/student-list.component.ts
@@ -4,7 +4,7 @@ import { StudentsService } from '../shared/services/students.service';
 @Component({
   selector: 'app-student-list',
   templateUrl: './student-list.component.html',
-  styleUrls: ['./student-list.component.css']
+  styleUrls: ['']
 })
 export class StudentListComponent implements OnInit {
 
